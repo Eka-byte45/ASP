6c1dbae5987eca97fd58a005d7f8735dd7560dfc
Екатерина Голумбиевская <golumbiyevskaya90@bk.ru>
2026-09-02 23:25:35 +0800

    hw in progress

---
+++ Blazor/Blazor/Components/Pages/Power.razor.cs
@@ -0,0 +1,36 @@
+﻿namespace Blazor.Components.Pages
+{
+    public partial class Power
+    {
+        int baseNumber;
+        int exponent;
+        double result;
+
+        void CalculatePower()
+        {
+            result = 1;
+            if (exponent == 0)
+            {
+                result = 1;
+            }
+            if (exponent < 0)
+            {
+                int plus = -exponent;
+
+                for (int i = 1; i <= plus; i++)
+                {
+                    result *= baseNumber;
+                }
+                result = 1 / result;
+            }
+
+            else
+            {
+                for (int i = 1; i <= exponent; i++)
+                {
+                    result *= baseNumber;
+                }
+            }
+        }
+    }
+}
