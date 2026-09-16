7b0ee4dbd0750b07db521937842e5f26d418dd22
Екатерина Голумбиевская <golumbiyevskaya90@bk.ru>
2026-09-17 00:49:07 +0800

    Groups & Directions Navigation properties

--- Blazor/Academy8/AcademyContext.cs
+++ Blazor/Academy8/AcademyContext.cs
@@ -2,7 +2,5 @@ using Microsoft.EntityFrameworkCore;
 
 public class AcademyContext(DbContextOptions<AcademyContext> options) : DbContext(options)
 {
-    public DbSet<Academy.Models.Discipline> Disciplines { get; set; } = default!;
     public DbSet<Academy.Models.Direction> Directions { get; set; } = default!;
-    public DbSet<Academy.Models.Group> Groups { get; set; } = default!;
 }
