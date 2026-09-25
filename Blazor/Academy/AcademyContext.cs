dfc97c1bacb0f40c7be13f96e91b355140af4ac4
Екатерина Голумбиевская <golumbiyevskaya90@bk.ru>
2026-09-26 01:51:59 +0800

    Lesson 2 - TODO Disciplines for Teacher

--- Blazor/Academy/AcademyContext.cs
+++ Blazor/Academy/AcademyContext.cs
@@ -6,5 +6,6 @@ public class AcademyContext(DbContextOptions<AcademyContext> options) : DbContex
     public DbSet<Academy.Models.Direction> Directions { get; set; } = default!;
     public DbSet<Academy.Models.Group> Groups { get; set; } = default!;
     public DbSet<Academy.Models.Student> Students { get; set; } = default!;
+    public DbSet<Academy.Models.Teacher> Teachers { get; set; } = default!;
     
 }
